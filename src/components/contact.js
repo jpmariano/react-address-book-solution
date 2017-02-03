4124da0d2ffc743c3d39273bceab0224527829da
JP de Vries <mail@devries.jp>
2017-02-02 22:54:23 -0800

    More Meaningful Alt Text on Contact Photos
    Now reads something like "Bob Bear's contact photo" instead of "Contact
    Photo" for each one.

--- src/components/contact.js
+++ src/components/contact.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Contact(props) {
     return (
         <div className="contact">
-            <img className="contact-photo" src={props.photo} alt="Contact photo" />
+            <img className="contact-photo" src={props.photo} alt={`${props.name}${props.name.slice(-1) == 's' ? "'" : "'s"} contact photo`} />
             <div className="contact-name">
                 {props.name}
             </div>
