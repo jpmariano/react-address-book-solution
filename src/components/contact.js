dc21afcebf4aaa22c66f386068259300d3fd0445
Joe Turner <joe@oampo.co.uk>
2017-02-03 08:36:03 +0000

    Fix warnings.

--- src/components/contact.js
+++ src/components/contact.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Contact(props) {
     return (
         <div id={`c${props.index}`} className="contact">
-            <img className="contact-photo" src={props.photo} alt={`${props.name}${props.name.slice(-1) == 's' ? "'" : "'s"} contact photo`}  />
+            <img className="contact-photo" src={props.photo} alt={`${props.name}${props.name.slice(-1) === 's' ? "'" : "'s"} avatar`}  />
             <div className="contact-name">
                 {props.name}
             </div>
