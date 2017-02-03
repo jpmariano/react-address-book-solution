231400fb145758aa0e58a08a27dc53f77caebc0e
JP de Vries <mail@devries.jp>
2017-02-02 23:28:14 -0800

    More Traversable DOM (flex-order)
    In this case we want our visual design to read contact photo, name,
    address. Ideally in the DOM, the contact name would be before the image
    and a heading. We can accomplish this with flexbox though!
    
    So now the order in the DOM is
    h2.contact-name
    img.contact-photo
    div.contact-address
    
    which is better for search engines and screen readers but our design
    still looks the same thanks to flex order.
    
    fixing CSS syntax

--- src/components/contact.js
+++ src/components/contact.js
@@ -4,9 +4,9 @@ export default function Contact(props) {
     return (
         <div className="contact">
             <img className="contact-photo" src={props.photo} alt="Contact photo" />
-            <div className="contact-name">
+            <h2 className="contact-name">
                 {props.name}
-            </div>
+            </h2>
             <div className="contact-address">
                 {props.address}
             </div>
